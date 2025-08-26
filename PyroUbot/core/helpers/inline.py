573f10e3c4e64abddc74307b02d23aa38c9595d2
ndyofcial <ndytelegram@gmail.com>
2025-08-27 00:03:12 +0700

    Update inline.py

--- PyroUbot/core/helpers/inline.py
+++ PyroUbot/core/helpers/inline.py
@@ -141,7 +141,7 @@ class BTN:
     
     # START -> diganti ke ReplyKeyboardMarkup
     def START(message):
-        if message.from_user.id != OWNER_ID:
+        if message.from_user.id = OWNER_ID:
             # MENU UNTUK USER
             button = [
                 [KeyboardButton("⦪ ᴛʀɪᴀʟ ⦫")],
